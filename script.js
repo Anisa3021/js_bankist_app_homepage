66f62013ffaaa0db7a7cf1475ff4632e0f84ca64
Anisa Majmari <anisamajmari@gmail.com>
2023-05-01 14:23:10 +0200

    smooth scroll

--- script.js
+++ script.js
@@ -65,3 +65,15 @@ tabsContainer.addEventListener("click", function (e) {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add("operations__content--active");
 });
+
+////////////////////////
+//Page navigation
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
