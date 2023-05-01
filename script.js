3f58c1134084d2294da20115cca14445df3dc103
Anisa Majmari <anisamajmari@gmail.com>
2023-05-01 14:33:19 +0200

    menu fade

--- script.js
+++ script.js
@@ -77,3 +77,22 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
 });
+
+////////////////////////////
+//Menu fade
+const handleHover = function (e) {
+  if (e.target.classList.contains("nav__link")) {
+    const link = e.target;
+
+    const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+    const logo = link.closest(".nav").querySelector("img");
+
+    siblings.forEach((element) => {
+      if (element !== link) element.style.opacity = this;
+    });
+
+    logo.style.opacity = this;
+  }
+};
+nav.addEventListener("mouseover", handleHover.bind(0.5));
+nav.addEventListener("mouseout", handleHover.bind(1));
