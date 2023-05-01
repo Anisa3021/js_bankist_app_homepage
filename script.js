7b014a6dc8ed65362f1ef48f9f87644a2eaca697
Anisa Majmari <anisamajmari@gmail.com>
2023-05-01 14:16:42 +0200

    tabbed component

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const section1 = document.querySelector("#section--1");
 const nav = document.querySelector(".nav");
 const tabs = document.querySelectorAll(".operations__tab");
 const tabsContainer = document.querySelector(".operations__tab-container");
-const tabsContent = document.querySelector(".operations__content");
+const tabsContent = document.querySelectorAll(".operations__content");
 
 /////////////////////////////
 //Modal window
@@ -42,4 +42,26 @@ btnScrollTo.addEventListener("click", function (e) {
   const s1coords = section1.getBoundingClientRect();
 
   section1.scrollIntoView({ behavior: "smooth" });
+});
+
+/////////////////////////////
+//Tabbed component
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab");
+
+  if (!clicked) return;
+
+  //Remove active classes
+  tabs.forEach((tab) => tab.classList.remove("operations__tab--active"));
+  tabsContent.forEach((content) =>
+    content.classList.remove("operations__content--active")
+  );
+
+  //Activ tab
+  clicked.classList.add("operations__tab--active");
+
+  //Activ content
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
 });
